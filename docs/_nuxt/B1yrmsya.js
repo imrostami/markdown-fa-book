7afe87dd0d4331eb0d7ebad58435f01e090862b2
mohammad <sapurtcomputer30@gmail.com>
2024-07-12 00:37:27 +0330

    docs published

---
+++ docs/_nuxt/B1yrmsya.js
@@ -0,0 +1,1 @@
+import{B as r,H as s,_ as c}from"./D-2sBEcE.js";import{_,c as l,b as o,r as m,o as d}from"./CHNqNXQb.js";import"./DLGN_vH8.js";const p={};function f(t,i){const e=r,n=s,a=c;return d(),l("div",null,[o(e,{class:"scrool-area"}),o(n,{lang:"fa",dir:"rtl","data-bs-theme":"dark"}),m(t.$slots,"default"),o(a)])}const x=_(p,[["render",f]]);export{x as default};
---
+++ docs/_nuxt/eObJC1iS.js
@@ -0,0 +1,1 @@
+import{u as n}from"./DLGN_vH8.js";import{s as u}from"./CHNqNXQb.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return n({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const s=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:s}}})}export{i as u};
