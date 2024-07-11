c2eec31c6777b04cd3e1f19bd41f882b841db0e1
mohammad <sapurtcomputer30@gmail.com>
2024-07-12 01:21:01 +0330

    build setting updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr:false,
   plugins: [
     '~/plugins/global-plugin.js'
   ],
